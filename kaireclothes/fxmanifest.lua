4e8536891d9580f6de8de32c5528f29ae1376470
kairepasc <107813926+kairepasc@users.noreply.github.com>
2022-11-15 08:22:26 +0800

    kaireclothes

---
+++ kaireclothes/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Generated with AltTool
+
+fx_version 'bodacious'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_kaireclothes.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kaireclothes.meta'
+
+files {
+  'mp_m_freemode_01_mp_m_kaireclothes.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kaireclothes.meta'
